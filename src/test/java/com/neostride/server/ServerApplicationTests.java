34f42233b3e0fd84b6b2f599f96aa62a58d1e5fd
snvnn <yunh1205@gmail.com>
2026-04-28 16:54:46 +0900

    first commit

---
+++ src/test/java/com/neostride/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neostride.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
