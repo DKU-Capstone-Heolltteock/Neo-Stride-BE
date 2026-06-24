c5ffebb1f5b9181c2f4e6bde16841dc9c4c92212
snvnn <yunh1205@gmail.com>
2026-06-24 10:22:12 +0900

    Keep rollback coaching paces representable

--- deploy/mysql/migrations/012_normalize_coaching_pace_seconds.down.sql
+++ deploy/mysql/migrations/012_normalize_coaching_pace_seconds.down.sql
@@ -11,5 +11,5 @@ UPDATE plans
 SET target_pace = ROUND(target_pace / 60, 6)
 WHERE target_pace IS NOT NULL AND target_pace >= 60;
 
-ALTER TABLE goals MODIFY COLUMN target_pace DECIMAL(8,6) NOT NULL;
-ALTER TABLE plans MODIFY COLUMN target_pace DECIMAL(8,6) NOT NULL;
+ALTER TABLE goals MODIFY COLUMN target_pace DECIMAL(10,6) NOT NULL;
+ALTER TABLE plans MODIFY COLUMN target_pace DECIMAL(10,6) NOT NULL;
